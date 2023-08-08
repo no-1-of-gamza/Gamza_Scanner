3cb62ea1df0dc8d0b8cc98706ac6269721e06e37
silver-liq9118 <silver-liq9118@naver.com>
2023-08-08 16:30:42 +0900

    tcp_port_scanner
    tcp_port_scanner

--- Gamza_Scanner.py
+++ Gamza_Scanner.py
@@ -4,14 +4,32 @@ import socket
 import random
 import sys
 import time
+import os
 
-def tcp_scan(host, port, thread_ids):
-    global task_count
+
+def print_welcome_message():
+    welcome_message = """
+    _____                                 _____                                        
+    |  __ \                               /  ___|                                       
+    | |  \/  __ _  _ __ ___   ____  __ _  \ `--.   ___   __ _  _ __   _ __    ___  _ __ 
+    | | __  / _` || '_ ` _ \ |_  / / _` |  `--. \ / __| / _` || '_ \ | '_ \  / _ \| '__|
+    | |_\ \| (_| || | | | | | / / | (_| | /\__/ /| (__ | (_| || | | || | | ||  __/| |   
+    \____/ \__,_||_| |_| |_|/___| \__,_| \____/  \___| \__,_||_| |_||_| |_| \___||_| 
+    Welcome to Port Scanner!
+    Scanning for Well-Known Port
+    """
+    #os.system('clear')  # 화면을 지우는 명령어 (Linux/Mac)
+    print(welcome_message)
+
+
+def tcp_scan(host, port,thread_ids):
+    
+    lock = threading.Lock()
+    
     with lock:
         thread_id = threading.get_ident()
         thread_ids.add(thread_id)
-        task_count += 1
-        task_number = task_count
+
     time.sleep(0.5)
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,9 +37,9 @@ def tcp_scan(host, port, thread_ids):
         result = sock.connect_ex((host, port))
         sock.close()
         if result == 0:
-            return task_number, thread_id, port, True
+            return thread_id, port, True
         else:
-            return task_number, thread_id, port, False
+            return thread_id, port, False
     except KeyboardInterrupt:
         print("\nExiting program.")
         sys.exit()
@@ -32,7 +50,7 @@ def tcp_scan(host, port, thread_ids):
         print("Couldn't connect to server.")
         sys.exit()
 
-def multi_threading(num_threads,thread_ids):
+def multi_threading(num_threads,thread_ids,target_host,ports):
 
     open_ports = []
     closed_ports = []
@@ -40,12 +58,14 @@ def multi_threading(num_threads,thread_ids):
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         futures = [executor.submit(tcp_scan, target_host, port, thread_ids) for port in ports]
         for future in concurrent.futures.as_completed(futures):
-            task_number, thread_id, port, is_open = future.result()
+            thread_id, port, is_open = future.result()
             if is_open:
                 open_ports.append(port)
             else:
                 closed_ports.append(port)
-            print(f"Task number {task_number}, Thread ID: {thread_id} has finished scanning port {port}.")
+            print( f"Thread ID: {thread_id} has finished scanning port {port}.")
+
+    result_printing(thread_ids,closed_ports,open_ports)
 
 
 def result_printing(thread_ids,closed_ports,open_ports):
@@ -54,18 +74,27 @@ def result_printing(thread_ids,closed_ports,open_ports):
 
     print(f"\nTotal used thread IDs: {len(thread_ids)}")
 
+    closed_ports.sort()  # Sort the closed ports list
+    open_ports.sort()    # Sort the open ports list
+
     print("\nClosed ports:")
     print(', '.join(map(str, closed_ports)))
 
     print("\nOpen ports:")
     print(', '.join(map(str, open_ports)))
 
-
     print(f"Total closed ports: {len(closed_ports)}")
     print(f"\nTotal open ports: {len(open_ports)}")
 
+def main():
 
-if __name__ == "__main__":
+    #아스키아트 출력
+    print_welcome_message()
+
+    # 사용자 입력값 example.py {ip}
+    if len(sys.argv) < 2:
+        print(" : python example.py YourIP!")
+        return
     #사용지 지정 포트
     start_num = 1
     end_num = 1024
@@ -74,15 +103,18 @@ if __name__ == "__main__":
     random.shuffle(ports)
 
     #Target_host IP
-    target_host = 'localhost'
+    target_host = 'localhost' #기본적으로 로컬 호스트
+    target_host = sys.argv[1]
+    # 입력값을 활용한 프로그램 로직 추가
+
 
     # 멀티스레딩 카운트 및 레이스컨디션 방지 글로벌 변수로 선언 해야함 
-    lock = threading.Lock()
-    task_count = 0
     #사용할 스레드 개수
-    num_threads = 10
+    num_threads = 10 #기본적으로 10개고정
     thread_ids = set()
-    multi_threading(num_threads,thread_ids)
-    tcp_scan(target_host,ports,thread_ids)
-    result_printing()
 
+    multi_threading(num_threads,thread_ids,target_host,ports)
+    tcp_scan(target_host,start_num,thread_ids,)
+
+if __name__ == "__main__":
+    main()
