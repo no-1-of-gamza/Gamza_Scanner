02244681b977dd7156f455c49e8727f684354313
silver-liq9118 <silver-liq9118@naver.com>
2023-08-08 21:47:10 +0900

    bannergrabing
    banner grabing

--- Gamza_Scanner.py
+++ Gamza_Scanner.py
@@ -6,6 +6,7 @@ import sys
 import time
 import os
 from tqdm import tqdm
+import json
 
 
 def print_welcome_message():
@@ -25,6 +26,14 @@ def print_welcome_message():
     #os.system('clear')  # 화면을 지우는 명령어 (Linux/Mac)
     print(welcome_message)
 
+def load_rule_set(json_file_path):
+    
+    with open(json_file_path, 'r') as file:
+        rule_data = json.load(file)
+    
+    print()
+
+
 
 def tcp_scan(host, port,thread_ids):
     
@@ -37,13 +46,14 @@ def tcp_scan(host, port,thread_ids):
     # time.sleep(0.5) TCP 연결 상태보고 조절 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
+        sock.settimeout(0.1)
         result = sock.connect_ex((host, port))
-        sock.close()
         if result == 0:
-            return thread_id, port, True
+            return thread_id, port, True, sock
         else:
-            return thread_id, port, False
+            sock.close()
+            sock = None
+            return thread_id, port, False, sock
     except KeyboardInterrupt:
         print("\nExiting program.")
         sys.exit()
@@ -54,8 +64,29 @@ def tcp_scan(host, port,thread_ids):
         print("Couldn't connect to server.")
         sys.exit()
 
-def multi_threading(num_threads,thread_ids,target_host,ports):
+def banner_grabbing(target_host, target_port, sock):
+    try:
+    # Get을 하지 않으면 서버에서 응답을 하지 않음 
+        sock.send(b'GET / HTTP/1.1\r\nHost: ' + target_host.encode() + b'\r\n\r\n')
+    # 배너 정보 수신
+        banner = sock.recv(1024).decode().strip()
+        banner_lines = banner.split('\n')
+        banner = banner_lines[:3]
+
+        print(f"{banner}")
+        return banner
     
+    except ConnectionRefusedError:
+        print(f"Connection refused: {target_host}:{target_port}")
+    except socket.timeout:
+        print(f"Connection timeout: {target_host}:{target_port}")
+    except socket.error as e:
+        print(f"Error: {e}")
+    finally:
+    # 소켓 종료
+        sock.close()
+
+def multi_threading(num_threads, thread_ids, target_host, ports):
     open_ports = []
     closed_ports = []
 
@@ -66,15 +97,13 @@ def multi_threading(num_threads,thread_ids,target_host,ports):
         with tqdm(total=total_ports, desc="Scanning Ports", unit="port") as pbar:
             for future in concurrent.futures.as_completed(futures):
                 pbar.update(1)
-                thread_id, port, is_open = future.result()
-                if is_open:
+                thread_id, port, is_open, sock = future.result()
+                if is_open or sock is not None:
                     open_ports.append(port)
+                    banner_grabbing(target_host, port, sock)
                 else:
                     closed_ports.append(port)
-                #print( f"has finished scanning port {port}") 
-                    
-
-    result_printing(thread_ids,closed_ports,open_ports)
+    result_printing(thread_ids, closed_ports, open_ports)
 
 def result_printing(thread_ids,closed_ports,open_ports):
     print("\nUsed thread IDs:")
@@ -91,18 +120,21 @@ def result_printing(thread_ids,closed_ports,open_ports):
 
     print("\nOpen ports:")
     print(', '.join(map(str, open_ports)))
+    print(f"Total open ports: {len(open_ports)}")
 
     print(f"Total closed ports: {len(closed_ports)}")
-    print(f"\nTotal open ports: {len(open_ports)}")
-
+    
 def main():
 
     #아스키아트 출력
     print_welcome_message()
 
+    #rule_set 경로
+    json_file_path = 'rule_set.json'
+
     # 사용자 입력값 example.py {ip}
     if len(sys.argv) < 2:
-        print(" : python example.py YourIP!")
+        print("\n python Gamza_Scanner.py YourIP!")
         return
     #사용지 지정 포트
     start_num = 1
@@ -112,18 +144,15 @@ def main():
     random.shuffle(ports)
 
     #Target_host IP
-    target_host = 'localhost' #기본적으로 로컬 호스트
     target_host = sys.argv[1]
-    # 입력값을 활용한 프로그램 로직 추가
 
-
-    # 멀티스레딩 카운트 및 레이스컨디션 방지 글로벌 변수로 선언 해야함 
     #사용할 스레드 개수
-    num_threads = 10 #기본적으로 16개고정
+    num_threads = 16 #기본적으로 16개고정
     thread_ids = set()
 
     multi_threading(num_threads,thread_ids,target_host,ports)
     tcp_scan(target_host,start_num,thread_ids)
+    print(f"\nDetected : {start_num} - {end_num} port in {target_host}\n")
 
 if __name__ == "__main__":
     main()
