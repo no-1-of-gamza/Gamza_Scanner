5f920c31aa3a5bed55765de421cd4201829ea2f9
silver-liq9118 <silver-liq9118@naver.com>
2023-08-08 17:22:29 +0900

    process Rate
    process Rate!

--- Gamza_Scanner.py
+++ Gamza_Scanner.py
@@ -5,18 +5,22 @@ import random
 import sys
 import time
 import os
+from tqdm import tqdm
 
 
 def print_welcome_message():
     welcome_message = """
-    _____                                 _____                                        
+    ______                                _______                                       
     |  __ \                               /  ___|                                       
     | |  \/  __ _  _ __ ___   ____  __ _  \ `--.   ___   __ _  _ __   _ __    ___  _ __ 
     | | __  / _` || '_ ` _ \ |_  / / _` |  `--. \ / __| / _` || '_ \ | '_ \  / _ \| '__|
     | |_\ \| (_| || | | | | | / / | (_| | /\__/ /| (__ | (_| || | | || | | ||  __/| |   
     \____/ \__,_||_| |_| |_|/___| \__,_| \____/  \___| \__,_||_| |_||_| |_| \___||_| 
+
     Welcome to Port Scanner!
+
     Scanning for Well-Known Port
+
     """
     #os.system('clear')  # 화면을 지우는 명령어 (Linux/Mac)
     print(welcome_message)
@@ -33,7 +37,7 @@ def tcp_scan(host, port,thread_ids):
     time.sleep(0.5)
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(0.5)
         result = sock.connect_ex((host, port))
         sock.close()
         if result == 0:
@@ -51,22 +55,26 @@ def tcp_scan(host, port,thread_ids):
         sys.exit()
 
 def multi_threading(num_threads,thread_ids,target_host,ports):
-
+    
     open_ports = []
     closed_ports = []
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         futures = [executor.submit(tcp_scan, target_host, port, thread_ids) for port in ports]
-        for future in concurrent.futures.as_completed(futures):
-            thread_id, port, is_open = future.result()
-            if is_open:
-                open_ports.append(port)
-            else:
-                closed_ports.append(port)
-            print( f"Thread ID: {thread_id} has finished scanning port {port}.")
+        total_ports = len(ports)
+
+        with tqdm(total=total_ports, desc="Scanning Ports", unit="port") as pbar:
+            for future in concurrent.futures.as_completed(futures):
+                pbar.update(1)
+                thread_id, port, is_open = future.result()
+                if is_open:
+                    open_ports.append(port)
+                else:
+                    closed_ports.append(port)
+                #print( f"has finished scanning port {port}") 
+                    
 
     result_printing(thread_ids,closed_ports,open_ports)
-
 
 def result_printing(thread_ids,closed_ports,open_ports):
     print("\nUsed thread IDs:")
@@ -74,11 +82,12 @@ def result_printing(thread_ids,closed_ports,open_ports):
 
     print(f"\nTotal used thread IDs: {len(thread_ids)}")
 
-    closed_ports.sort()  # Sort the closed ports list
-    open_ports.sort()    # Sort the open ports list
+    closed_ports.sort()  
+    open_ports.sort()   
 
-    print("\nClosed ports:")
-    print(', '.join(map(str, closed_ports)))
+    #closed 포트 비출력
+    #print("\nClosed ports:")
+    #print(', '.join(map(str, closed_ports)))
 
     print("\nOpen ports:")
     print(', '.join(map(str, open_ports)))
@@ -110,11 +119,11 @@ def main():
 
     # 멀티스레딩 카운트 및 레이스컨디션 방지 글로벌 변수로 선언 해야함 
     #사용할 스레드 개수
-    num_threads = 10 #기본적으로 10개고정
+    num_threads = 30 #기본적으로 16개고정
     thread_ids = set()
 
     multi_threading(num_threads,thread_ids,target_host,ports)
-    tcp_scan(target_host,start_num,thread_ids,)
+    tcp_scan(target_host,start_num,thread_ids)
 
 if __name__ == "__main__":
     main()
